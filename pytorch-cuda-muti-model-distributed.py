49567cddfbd669e921851bae824871ca2768520c
Chang Yang <128663575+techlead-krischang@users.noreply.github.com>
2023-03-24 17:17:27 +0800

    Update pytorch-cuda-muti-model-distributed.py

--- pytorch-cuda-muti-model-distributed.py
+++ pytorch-cuda-muti-model-distributed.py
@@ -12,6 +12,8 @@ os.environ['WORLD_SIZE'] = '8'  # 设置进程数量为 8
 os.environ['MASTER_ADDR'] = '127.0.0.1'
 os.environ['MASTER_PORT'] = '49159'
 
+print("setting ready");
+
 batch_size = 64
 learning_rate = 0.01
 num_epochs = 10
